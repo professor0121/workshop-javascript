712566a04c968e21c0a9c25d9de163e83e1fe43a
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-10-16 12:15:53 +0530

    task is done

--- 10-15-2025/src/App.js
+++ 10-15-2025/src/App.js
@@ -3,25 +3,50 @@ import About from './components/about';
 import Home from './components/home';
 import ApiDataRendering from './components/apiDataRendering';
 import { Cart } from "./components/cart"
+import Layout from './components/layout';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import ProductDetails from './components/productDetails';
+
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [products, setProducts]=useState([]);
+  console.log(products);
+  // console.log(cart)
   return (
-    <div 
-    style={{
-      display:"flex",
-      width:"100vw"
-    }}
-    >
-      <ApiDataRendering
-        cart={cart}
-        setCart={setCart}
-      />
-      <Cart
-        cart={cart}
-        setCart={setCart}
-      />
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+            <Route path='/all-product'
+              element={
+                <ApiDataRendering
+                  cart={cart}
+                  setCart={setCart}
+
+                  products={products}
+                  setProducts={setProducts}
+                  
+                />
+              } />
+            <Route path='/cart'
+              element={
+                <Cart
+                  cart={cart}
+                  setCart={setCart}
+                />
+              } />
+            <Route path='/product-details'
+              element={
+                <ProductDetails
+                  cart={cart}
+                  setCart={setCart}
+                   products={products}
+                />
+              } />
+
+          </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
